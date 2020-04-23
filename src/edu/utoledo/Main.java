72f1a2e3b9a8923fbac41a1232016978a1c6772f
donmiller <don@groundspeedhq.com>
2020-04-22 22:58:55 -0400

    Added calculator method

--- src/edu/utoledo/Main.java
+++ src/edu/utoledo/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("2+2=" + addTwoPlusTwo());
+    }
+
+    public static int addTwoPlusTwo() {
+        return 2 + 2;
     }
 }
